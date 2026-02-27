59fab6aff1a96c5c6c22c68f5c0891d9da392a8f
Developer <test@example.com>
2026-02-27 10:48:52 -0500

    Revert "Primer commit proyecto PokeAPI angular"
    This reverts commit 8545be45fe993f723f4e0576e201d8aac1c473d8.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { PokemonComponent} from './pokemon/pokemon.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [PokemonComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
